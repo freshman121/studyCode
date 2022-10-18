d50cbfcfe7aa63a71b6b3f937ca99348dd514d73
freshman121 <1113553385@qq.com>
2022-10-18 13:23:25 +0800

    第一次修改

--- src/Main.java
+++ src/Main.java
@@ -15,5 +15,7 @@ public class Main {
         System.out.printf("HH:MM:SS格式（24时制）：%tT%n",date);
         //R的使用
         System.out.printf("HH:MM格式（24时制）：%tR",date);
+
+        System.out.println("add content");
     }
 }
